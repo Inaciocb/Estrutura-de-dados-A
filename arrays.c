aa49e9f2907a288d26b27a04fc0e72480cbb06c1
Inácio <67322986+Inaciocb@users.noreply.github.com>
2023-03-31 19:07:32 -0300

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -7,15 +7,24 @@ int main()
     {
         for(j = 0; j < 3; j++)
         {
-            mat[i][j] = i;
+            mat[i][j] = (j+1+i);
         }
     }
+    
     int mat2[12];
+    
+    /*
+    
+     vetor mat2 recebe os valores conforme a fórmula 
+            "v[k] = mat[i][j] onde k = n * i + j em que n é o número de
+             colunas da matriz bidimensional"
+   
+   */
     for(i = 0; i < 4; i++)
     {
         for(j = 0; j < 3; j++)
         {
-            mat2[4*i] = mat[i][j];
+            mat2[3*i+j] = mat[i][j];
         }
     }
     for(i = 0 ; i < 12; i++)
